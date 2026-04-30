28abdf9e6e1b5c2836884b34549d51ba33a30186
xchalox <gonzalo.letelier@mail.udp.cl>
2026-04-30 00:48:29 -0400

    prueba de rama

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <!--begin::Head-->
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>AdminLTE v4 | Dashboard</title>
+    <title>Programacion web en PHP con AdminLTE </title>
 
     <!--begin::Accessibility Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
